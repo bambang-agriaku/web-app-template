dbf48cf7dac705a99e9f70bcde76bfd604740913
Bambang Adi Prakoso <bambang.prakoso@agriaku.com>
2025-06-20 14:15:44 +0700

    specify env when building

---
+++ src/config/paths.ts
@@ -0,0 +1,16 @@
+export const paths = {
+  auth: {
+    login: {
+      path: "/auth/login",
+      getHref: () => "/auth/login",
+    },
+    me: {
+      path: "/auth/me",
+      getHref: () => "/auth/me",
+    },
+    refresh: {
+      path: "/auth/refresh",
+      getHref: () => "/auth/refresh",
+    },
+  },
+};
