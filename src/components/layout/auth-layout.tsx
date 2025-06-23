8e9a957318184062ac492458c55d70b3f4396ebe
Bambang Adi Prakoso <bambang.prakoso@agriaku.com>
2025-06-23 16:31:15 +0700

    adjust sidebar component structure

--- src/components/layout/auth-layout.tsx
+++ src/components/layout/auth-layout.tsx
@@ -1,7 +1,4 @@
-import { useAuth } from "@/lib/auth";
-import { Route } from "@/routes/_auth";
 import { Box } from "@mui/material";
-import { useRouter } from "@tanstack/react-router";
 import type { ReactNode } from "react";
 import { Sidebar } from "@/components/ui/sidebar";
 
@@ -9,26 +6,14 @@ type Props = {
   children: ReactNode;
 };
 
-export const AuthLayout = ({ children }: Props) => {
-  const router = useRouter();
-  const navigate = Route.useNavigate();
-  const auth = useAuth();
-
-  const handleLogout = async () => {
-    await auth.logout();
-    await router.invalidate();
-    navigate({ to: "/login", replace: true });
-  };
-
-  return (
-    <Box sx={{ display: "flex" }}>
-      <Sidebar />
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", p: 5 }}
-      >
-        {children}
-      </Box>
+export const AuthLayout = ({ children }: Props) => (
+  <Box sx={{ display: "flex" }}>
+    <Sidebar />
+    <Box
+      component="main"
+      sx={{ flexGrow: 1, bgcolor: "background.default", p: 5 }}
+    >
+      {children}
     </Box>
-  );
-};
+  </Box>
+);
--- src/components/ui/sidebar/sidebar.tsx
+++ src/components/ui/sidebar/sidebar.tsx
@@ -1,10 +1,23 @@
-import { Toolbar, Drawer, Divider, List } from "@mui/material";
+import { Toolbar, Drawer, Divider, List, Box, Button } from "@mui/material";
 import { ListItem } from "@/components/ui/sidebar";
 import { resources } from "@/lib/resource";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useAuth } from "@/lib/auth";
+import { useRouter, useNavigate } from "@tanstack/react-router";
 
 const drawerWidth = 280;
 
 const Sidebar = () => {
+  const router = useRouter();
+  const navigate = useNavigate();
+  const auth = useAuth();
+
+  const handleLogout = async () => {
+    await auth.logout();
+    await router.invalidate();
+    navigate({ to: "/login", replace: true });
+  };
+
   return (
     <Drawer
       sx={{
@@ -13,18 +26,34 @@ const Sidebar = () => {
         "& .MuiDrawer-paper": {
           width: drawerWidth,
           boxSizing: "border-box",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
         },
       }}
       variant="permanent"
       anchor="left"
     >
-      <Toolbar />
-      <Divider />
-      <List>
-        {resources.map((resource) => (
-          <ListItem key={resource.name} resource={resource} />
-        ))}
-      </List>
+      <Box>
+        <Toolbar />
+        <Divider />
+        <List>
+          {resources.map((resource) => (
+            <ListItem key={resource.name} resource={resource} />
+          ))}
+        </List>
+      </Box>
+      <Box sx={{ p: 2 }}>
+        <Button
+          variant="outlined"
+          color="error"
+          startIcon={<LogoutIcon />}
+          fullWidth
+          onClick={handleLogout}
+        >
+          Logout
+        </Button>
+      </Box>
     </Drawer>
   );
 };
